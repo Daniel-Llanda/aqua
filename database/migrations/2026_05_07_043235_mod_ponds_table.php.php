910831073360bb481fb6a6b0b30cb36f46a80ec6
yowizem <penakierby@gmail.com>
2026-05-07 12:33:59 +0800

    Gawin

---
+++ database/migrations/2026_05_07_043235_mod_ponds_table.php.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ponds', function (Blueprint $table) {
+            //
+            $table->date('hatching_date')->nullable()->change();
+            $table->json('species_data')->nullable()->change();
+            $table->json('harvest_data')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
